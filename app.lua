638cf54a1a992e8be27f09dee305d74f92d11a13
application <application@unixcape-core>
2017-09-09 17:13:17 +0700

    edit app function directory

--- app.lua
+++ app.lua
@@ -4,7 +4,7 @@ end
  freeswitch.consoleLog("notice", XML_REQUEST['section'])
 if XML_REQUEST['section'] == 'directory' then
     if params:getHeader("action") == "sip_auth" then
-        local directory = require("directory")
+        local directory = require("uxcore/directory")
         directory.set(params)
         XMLSTRING = directory.getXML()
         --freeswitch.consoleLog("notice",params:serialize())
