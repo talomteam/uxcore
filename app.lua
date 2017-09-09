43b2e9a1a5fce8af5d16e765f9bd072d845d95d7
application <application@unixcape-core>
2017-09-09 15:30:00 +0700

    add app

--- app.lua
+++ app.lua
@@ -1,4 +1,4 @@
- freeswitch.consoleLog("notice", "SECTION " .. XML_REQUEST["section"] .. "\n")
+--[[ freeswitch.consoleLog("notice", "SECTION " .. XML_REQUEST["section"] .. "\n")
 If XML_REQUEST['section'] == 'directory' then
   freeswitch.consoleLog("notice",params:serialize())
 end
@@ -10,3 +10,5 @@ end
 if XML_REQUEST['section'] == 'configuration' then
   freeswitch.consoleLog("notice",params:serialize())
 end
+]]
+freeswitch.consoleLog("notice", "Debug from gen_dir_user_xml.lua, provided params:\n" .. params:serialize() .. "\n")
