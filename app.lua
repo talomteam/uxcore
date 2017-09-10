0a5404b03c8d8cf6b542d070ec560f844d3a7896
root <root@unixcape-core>
2017-09-10 14:26:14 +0700

    add lib network

--- app.lua
+++ app.lua
@@ -1,12 +1,14 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+local json = require("/lib/json")
 if XML_REQUEST == nil then
     return
 end
  freeswitch.consoleLog("notice", XML_REQUEST['section'])
 if XML_REQUEST['section'] == 'directory' then
     if params:getHeader("action") == "sip_auth" then
-        local directory = require("uxcore/directory")
-        directory.set(params)
-        XMLSTRING = directory.getXML()
+        local directory = require("directory")
+        
         --freeswitch.consoleLog("notice",params:serialize())
     end
 end
